f247e362479676b78478a96c62da16d016b01368
Tirtharaj Pramanik <tirtharajpramanik@icloud.com>
2022-07-25 19:46:14 +0530

    Added Todo Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor() {}
   title: string = 'todo-app';
+  todo: FormControl = new FormControl('');
+  todos: string[] = [];
+  newTodo() {
+    if (this.todo && this.todo.value) this.todos.push(this.todo.value);
+    this.todo.setValue('');
+  }
 }
